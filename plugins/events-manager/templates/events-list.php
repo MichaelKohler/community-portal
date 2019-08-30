6dd25f2b5de49c8652f350771c581d499583097b
Ky Capstick <ky.capstick@gmail.com>
2019-08-30 16:04:02 -0400

    pull in event card details

--- plugins/events-manager/templates/events-list.php
+++ plugins/events-manager/templates/events-list.php
@@ -11,11 +11,74 @@
     ?>
     <?php
     $args = apply_filters('em_content_events_args', $args);
-    
-    var_dump($args);
+    $events = EM_Events::get($args);
+    $months = array(
+      '01' => 'Jan',
+      '02' => 'Feb',
+      '03' => 'Mar',
+      '04' => 'Apr',
+      '05' => 'May',
+      '06' => 'Jun',
+      '07' => 'Jul',
+      '08' => 'Aug',
+      '09' => 'Sep',
+      '10' => 'Oct',
+      '11' => 'Nov',
+      '12' => 'Dec',
+    );
 
-    if( get_option('dbem_css_evlist') ) echo "<div class='css-events-list'>";
+    for ($i = 0; $i < count($events); $i = $i + 1) {
+      $location = em_get_location($events[$i]->location_id);
+      ?>
+        <div class="card">
+        <!-- <a href="<?php echo get_site_url('', $events[$i]->slug)?>"> -->
+        <?php 
+            $imgUrl = wp_get_attachment_url( get_post_thumbnail_id($events[$i]->post_id));
+            if ($imgUrl) {
+              ?>
+              <img src="<?php echo $imgUrl ?>" alt="">
+              <?php
+            }
+          ?>
+          <?php 
+            $month = substr($events[$i]->start_date, 5, 2);
+            $date = substr($events[$i]->start_date, 8, 2);
+            $year = substr($events[$i]->start_date, 0, 4);
+          ?>
+          <p><span><?php echo $months[$month] ?> </span><span><?php echo $date ?></span></p>
+          <h2><?php echo $events[$i]->event_name; ?></h2>
+          <p><?php echo $months[$month].' '.$date.', '.$year.' @ '.substr($events[$i]->event_start_time, 0, 5).' - '.substr($events[$i]->event_end_time, 0, 5).' '.$events[$i]->event_timezone; ?></p>
+          <p><?php
+            if ($location->address) {
+              echo $location->address.' - '; 
+            }
+            if ($location->town) {
+              echo $location->town;
+              if ($location->country) {
+                echo ', '.$location->country;
+              }
+            } else {
+              echo $location->country;
+            }
+          ?></p>
+          <?php 
+            $tags = get_the_terms($events[$i]->post_id, EM_TAXONOMY_TAG);
+            if ($tags) {
+              ?>
+              <ul>
+                <?php
+                  for ($j=0; $j < count($tags); $j = $j + 1) {
+                    ?>
+                    <li><?php echo $tags[$j]->name ?></li>
+                    <?php
+                  }
+                ?>
+                </ul>
+              <?php
+            }
+          ?>
+          <!-- </a> -->
+        </div>
+      <?php
+    }
     
-    echo EM_Events::output( $args );
-    
-    if( get_option('dbem_css_evlist') ) echo "</div>";
